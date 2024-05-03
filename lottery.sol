3bf4634a6dec18520a55bede4920755973ce5450
Henry <101131194+henrygagnier@users.noreply.github.com>
2024-05-02 21:56:29 -0400

    feat: misc changes

--- lottery.sol
+++ lottery.sol
@@ -1,43 +1,88 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import "./randomNumberGenerator.sol";
+import {VRFCoordinatorV2Interface} from "@chainlink/contracts@1.1.0/src/v0.8/vrf/interfaces/VRFCoordinatorV2Interface.sol";
+import {VRFConsumerBaseV2} from "@chainlink/contracts@1.1.0/src/v0.8/vrf/VRFConsumerBaseV2.sol";
 
-contract TetradLottery is RandomNumberGenerator {
-    struct Lottery {
-        uint256[6] rewardsBreakdown; // 0: 1 matching number // 5: 6 matching numbers
-        uint256 treasuryFee; // 500: 5% // 200: 2% // 50: 0.5
-        uint256[6] cakePerBracket;
-        uint256[6] countWinnersPerBracket;
-        uint256 firstTicketId;
-        uint256 firstTicketIdNextLottery;
-        uint256 amountCollected;
-        uint32 finalNumber;
+contract RandomNumberGenerator is VRFConsumerBaseV2 {
+    event RequestSent(uint256 requestId, uint32 numWords);
+    event RequestFulfilled(uint256 requestId, uint256[] randomWords);
+
+    struct RequestStatus {
+        bool fulfilled;
+        bool exists;
+        uint256[] randomWords;
+    }
+    mapping(uint256 => RequestStatus)
+        public requests;
+    VRFCoordinatorV2Interface COORDINATOR;
+
+    uint64 subscriptionId;
+    uint256[] public requestIds;
+    uint256 public lastRequestId;
+
+    // https://docs.chain.link/docs/vrf/v2/subscription/supported-networks/#configurations
+    bytes32 keyHash =
+         0x027f94ff1465b3525f9fc03e9ff7d6d2c0953482246dd6ae07570c45d6631414;
+
+    // Depends on the number of requested values that you want sent to the
+    // fulfillRandomWords() function. Storing each word costs about 20,000 gas,
+    // so 100,000 is a safe default for this example contract. Test and adjust
+    // this limit based on the network that you select, the size of the request,
+    // and the processing of the callback request in the fulfillRandomWords()
+    // function.
+    uint32 callbackGasLimit = 100000;
+
+    uint16 requestConfirmations = 3;
+
+    constructor(
+        uint64 _subscriptionId, address _coordinator
+    )
+        VRFConsumerBaseV2(_coordinator)
+    {
+        COORDINATOR = VRFCoordinatorV2Interface(
+            _coordinator
+        );
+        subscriptionId = _subscriptionId;
     }
 
-    mapping(uint256 => Lottery) lotteries;
-    uint256 public price = 500000000000000;
-
-    constructor(uint64 _subscriptionId, address _coordinator)
-        RandomNumberGenerator(_subscriptionId, _coordinator)
-    {}
-
-    struct Ticket {
-        uint32 number;
-        address owner;
+    function generate()
+        internal
+        returns (uint256 requestId)
+    {
+        requestId = COORDINATOR.requestRandomWords(
+            keyHash,
+            subscriptionId,
+            requestConfirmations,
+            callbackGasLimit,
+            1
+        );
+        requests[requestId] = RequestStatus({
+            randomWords: new uint256[](0),
+            exists: true,
+            fulfilled: false
+        });
+        requestIds.push(requestId);
+        lastRequestId = requestId;
+        emit RequestSent(requestId, 1);
+        return requestId;
     }
 
-    function buyTickets(uint32[] calldata _numbers) external payable {
-        if (_numbers.length == 0) revert();
-        if (_numbers.length * price != msg.value) revert();
-
-        uint256 id = block.timestamp / 24 hours;
+    function fulfillRandomWords(
+        uint256 _requestId,
+        uint256[] memory _randomWords
+    ) internal override {
+        require(requests[_requestId].exists, "request not found");
+        requests[_requestId].fulfilled = true;
+        requests[_requestId].randomWords = _randomWords;
+        emit RequestFulfilled(_requestId, _randomWords);
     }
 
-    function claimTickets(uint256 _id) external {
-        
-    }
-    function drawLottery(uint256 _id) external {
-        
+    function getRequestStatus(
+        uint256 _requestId
+    ) external view returns (bool fulfilled, uint256[] memory randomWords) {
+        require(requests[_requestId].exists, "request not found");
+        RequestStatus memory request = requests[_requestId];
+        return (request.fulfilled, request.randomWords);
     }
 }
